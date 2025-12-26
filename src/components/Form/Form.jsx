70d3eff4603b7db8dc92c88b18eef4938bc16961
said-oulcaid <118765538+said-oulcaid@users.noreply.github.com>
2025-12-26 13:04:35 +0100

    first commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,6 @@
+import "./Form.css";
+const Form = () => {
+  return <div>Form</div>;
+};
+
+export default Form;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,6 @@
+import "./Table.css";
+const Table = () => {
+  return <div>Table</div>;
+};
+
+export default Table;
